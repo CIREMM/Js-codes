935adac5a5215030e3a758cbf8e5e234d2edcd44
CIREMM <maingieric21@gmail.com>
2023-01-13 13:52:33 +0300

    committing files

---
+++ Mosh/index.js
@@ -0,0 +1,14 @@
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+//Calculate value
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
+
+greet('Eric', 'Maingi');
+greet('Maingi');
